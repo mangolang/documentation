63d0febabf69c39dc171c98546349e00742e12a1
Mark <mverleg.noreply@gmail.com>
2019-04-22 15:29:37 +0200

    Add pip requirements and remove "source" from path

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Mark'
 author = 'Mark'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,7 +34,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
